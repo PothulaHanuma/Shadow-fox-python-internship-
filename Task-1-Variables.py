96a887666d80c4c2752afb2e7077c8e7f955d72e
PothulaHanuma <hanumapothula8@gmail.com>
2026-09-10 19:44:15 +0530

    Add Task 1 - Variables implementation

---
+++ Task-1-Variables.py
@@ -0,0 +1,28 @@
+# ShadowFox Python Developer Internship
+# Task 1 - Variables
+
+# Question 1
+pi = 22 / 7
+
+print("Value of pi:", pi)
+print("Data type:", type(pi))
+
+
+# Question 2
+# 'for' cannot be used as a variable name
+# because 'for' is a Python keyword.
+
+print("Cannot use 'for' as a variable name because it is a Python keyword.")
+
+
+# Question 3
+principal = 10000
+rate = 5
+time = 3
+
+simple_interest = (principal * rate * time) / 100
+
+print("Principal:", principal)
+print("Rate:", rate)
+print("Time:", time)
+print("Simple Interest:", simple_interest)
